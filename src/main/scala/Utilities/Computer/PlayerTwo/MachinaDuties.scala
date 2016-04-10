381237ae8792b82812df9f8d7758c520f8b42e5f
GodspeedConor <cxj3042@rit.edu>
2016-04-10 19:07:22 -0400

    Fixed the check for losing moves to add plurality.  Searches for all bad moves now.

--- src/main/scala/Utilities/Computer/PlayerTwo/MachinaDuties.scala
+++ src/main/scala/Utilities/Computer/PlayerTwo/MachinaDuties.scala
@@ -3,6 +3,8 @@ package Utilities.Computer.PlayerTwo
 
 import lib.Support
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by Chloe on 4/8/16.
   * This file houses all the supporting functions for the AI controller
@@ -40,6 +42,7 @@ class MachinaDuties {
   /**
     * This function checks the board for the AI's potential win move.
     * The AI then knows where to place to win
+ *
     * @param my_board the current game board
     * @return -1 for no winning move, integer 0 to 6 inclusive for winning placement
     */
@@ -63,10 +66,12 @@ class MachinaDuties {
   /**
     * This function checks the board for the AI's potential losing move.
     * The AI then can avoid bad moves
-    * @param my_board
-    * @return
+    * @param my_board the current game board
+    * @return -1 for no losing move, integer 0 to 6 inclusive for losing placement
     */
-  def checkLosingMove(my_board: GameBoard.Board): Int={
+  def checkLosingMoves(my_board: GameBoard.Board): ArrayBuffer[Int] ={
+
+    val bad_moves = ArrayBuffer[Int]()
 
     for(column <- 0 to 6) {
 
@@ -76,10 +81,10 @@ class MachinaDuties {
       library.placeChip(column, 2, new_board)
 
       if (library.checkWinner(new_board) == 1) {
-        return column
+        bad_moves.append(column)
       }
     }
 
-    -1
+    bad_moves
   }
 }
